c17dc9839742c3a0bd3f015f9fcc8df0a980dbab
Corentin Girard <corentingirard.dev@gmail.com>
2018-04-15 16:00:17 +0200

    Prepare for start

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace SpritesheetMaker
-{
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace SpritesheetMaker {
+
+    public partial class MainWindow : Window {
+
+        public MainWindow() {
             InitializeComponent();
+        }
+
+        private void FileList_OnDrop_Drop(object sender, DragEventArgs e) {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop)) {
+                MessageBox.Show("This is not a good format!", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var files = (e.Data.GetData(DataFormats.FileDrop) as string[])?.Select(Path.GetFileName)
+                .OrderBy(file => file).ToArray();
+
+            if (files == null || files.Any(file => !file.Contains(".png"))) {
+                MessageBox.Show("Invalid files", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            FileList.ItemsSource = files;
+        }
+
+        private void MakeButton_OnClick(object sender, RoutedEventArgs e) {
+            
         }
     }
 }
